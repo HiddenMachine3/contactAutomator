2703ab9eba35d9625d297c04b0455c489200507a
hidden_machine(Suhas) <suhashmsocial@gmail.com>
2023-05-24 22:38:15 +0530

    update

--- main.py
+++ main.py
@@ -11,16 +11,11 @@ i=0
 for contact in contacts:
     print(i)
     i+=1
-    # subprocess.run(['adb', 'shell', 'am', 'force-stop', 'com.android.contacts'])
-    time.sleep(1)
     name = contact['name']
     name = name.replace(" ",  "_")
     name += "_RVU_neural_nexus"
     number = contact['number']
-    # subprocess.run(['adb', 'shell', 'am', 'start', '-a',
-    # 'android.intent.action.INSERT', '-t',
-    # 'vnd.android.cursor.dir/contact', '-e',
-    # 'name', name, '-e', 'phone', number])
+
 
     email = contact['email']
     subprocess.run(['adb', 'shell', 'am', 'start',
@@ -28,17 +23,15 @@ for contact in contacts:
                     '-t','vnd.android.cursor.dir/contact',
                     '-e','name', name, '-e', 'phone', number, '-e', 'email', email])
     time.sleep(1)
-    # subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_MENU'])  # Open the options menu
-    # subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_DPAD_DOWN'])  # Navigate to the "Save" option
-    subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_ENTER'])  # Select the "Save" option
-    subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_BACK'])  # Go back to the main Contacts screen
+    subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_ENTER'])
+    subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_BACK'])
     subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_ENTER'])
     subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_ENTER'])
     subprocess.run(['adb', 'shell', 'input', 'keyevent','KEYCODE_BACK'])
     subprocess.run(['adb', 'shell', 'input', 'keyevent','KEYCODE_BACK'])
     subprocess.run(['adb', 'shell', 'input', 'keyevent', 'KEYCODE_ENTER'])
     subprocess.run(['adb', 'shell', 'input', 'keyevent',
-                    'KEYCODE_BACK'])  # Go back to the contact creation screen for the next contact
+                    'KEYCODE_BACK'])
     subprocess.run(['adb', 'shell', 'input', 'keyevent',
-                    'KEYCODE_BACK'])  # Go back to the contact creation screen for the next contact
+                    'KEYCODE_BACK'])
     time.sleep(1)
